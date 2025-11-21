5217bff47f0e6ba2ae037ca3da8c4df883a53fa7
LoboBranco04 <93484482+LoboBranco04@users.noreply.github.com>
2025-11-21 00:35:12 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando!");
+});
+
+app.listen(3000, () => console.log("Rodando na porta 3000"));
